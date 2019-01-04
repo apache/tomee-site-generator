4ecb11d6d31fb979b3006dc7c85c9aedd693b42e
CesarHernandezGt <cfhernan123@gmail.com>
2019-01-03 21:51:36 -0600

    TOMEE-2444 (WIP) initial work

--- src/main/java/org/apache/tomee/website/Example.java
+++ src/main/java/org/apache/tomee/website/Example.java
@@ -17,6 +17,8 @@
 package org.apache.tomee.website;
 
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Example {
     private final File srcReadme;
@@ -26,6 +28,12 @@ public class Example {
     private final String href;
     private final String category;
 
+    public String getLanguage() {
+        return language;
+    }
+
+    private String language="";
+
     public Example(final File srcReadme, final String name, final String ext, final String href, final String category) {
         this.srcReadme = srcReadme;
         this.name = name;
@@ -33,9 +41,26 @@ public class Example {
         this.href = href;
         this.category = category;
     }
+    public Example(final File srcReadme, final String name, final String ext, final String href, final String category, final String language) {
+        this.srcReadme = srcReadme;
+        this.name = name;
+        this.ext = ext;
+        this.href = href;
+        this.category = category;
+        this.language = language;
+    }
 
     public void updateDestination(final File examplesDir) {
-        this.setDestReadme(new File(examplesDir, this.getName() + "." + this.getExt()));
+        //this.setDestReadme(new File(examplesDir, this.getName() + "." + this.getExt()));
+        if(this.language.equalsIgnoreCase("")){
+            this.setDestReadme(new File(examplesDir, this.getName() + "." + this.getExt()));
+        }else{
+
+            File languageDir = new File(examplesDir+"/es");
+            languageDir.mkdirs();
+
+            this.setDestReadme(new File(languageDir.getAbsolutePath(), this.getName() + "." + this.getExt()));
+        }
     }
 
     public File getDestReadme() {
@@ -69,8 +94,25 @@ public class Example {
     public static Example from(final File readme) {
         final String ext = getExtension(readme);
         final String exampleName = readme.getParentFile().getName();
+        final String language = getLanguage(readme);
 
-        return new Example(readme, exampleName, ext, exampleName + ".html", "Example");
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>aqui:"+exampleName);
+        if(exampleName.equalsIgnoreCase("")){
+            return new Example(readme, exampleName, ext, exampleName + ".html", "Example");
+        }else{
+            //return new Example(readme, exampleName+"-"+language, ext, exampleName+"-"+language + ".html", "Example/"+language);
+            return new Example(readme, exampleName, ext, exampleName + ".html", "Ejemplos",language);
+        }
+    }
+
+    private static String getLanguage(File readme) {
+        Pattern ptn = Pattern.compile("_(.*?)\\.");
+        Matcher matcher = ptn.matcher(readme.getName());
+        if(matcher.find()){
+            return matcher.group(1);
+        }else{
+            return "";
+        }
     }
 
     public static String getExtension(final File readme) {
