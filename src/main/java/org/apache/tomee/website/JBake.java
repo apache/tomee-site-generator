e195946a120829698d43ada3c7e8fff239ddbb8f
David Blevins <david.blevins@gmail.com>
2019-01-04 21:29:16 -0800

    Update repo locations after gitbox move.

--- src/main/java/org/apache/tomee/website/JBake.java
+++ src/main/java/org/apache/tomee/website/JBake.java
@@ -41,10 +41,10 @@ public class JBake {
                 new File("repos"),
                 new File("src/main/jbake"),
                 destination,
-                new Source("https://git-wip-us.apache.org/repos/asf/tomee.git", "master", "tomee-8.0", true),
-                new Source("https://git-wip-us.apache.org/repos/asf/tomee.git", "tomee-7.1.0", "tomee-7.1"),
-                new Source("https://git-wip-us.apache.org/repos/asf/tomee.git", "tomee-7.0.5", "tomee-7.0"),
-                new Source("https://git-wip-us.apache.org/repos/asf/tomee.git", "master", "master")
+                new Source("https://gitbox.apache.org/repos/asf/tomee.git", "master", "tomee-8.0", true),
+                new Source("https://gitbox.apache.org/repos/asf/tomee.git", "tomee-7.1.0", "tomee-7.1"),
+                new Source("https://gitbox.apache.org/repos/asf/tomee.git", "tomee-7.0.5", "tomee-7.0"),
+                new Source("https://gitbox.apache.org/repos/asf/tomee.git", "master", "master")
         );
 
         sources.prepare();
