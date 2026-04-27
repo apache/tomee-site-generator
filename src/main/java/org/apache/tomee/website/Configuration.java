a6c54061af0add3938811e5b9d9cb278a3800f10
Richard Zowalla <rzo1@apache.org>
2026-04-27 19:39:36 +0200

    Use v10 tag for jakartaee-platform clone
    The eclipse-ee4j/jakartaee-platform repo doesn't have a v10.0 ref;
    the Jakarta EE 10 release is tagged v10. Using the wrong ref made
    the bake fail at clone with 'Remote branch v10.0 not found'.

--- src/main/java/org/apache/tomee/website/Configuration.java
+++ src/main/java/org/apache/tomee/website/Configuration.java
@@ -204,7 +204,7 @@ public class Configuration {
             //    new Source("https://github.com/apache/tomee.git", "tomee-7.1.x", "tomee-7.1", false).javadoc("^org.apache.(openejb|tomee).*"),
           //      new Source("https://github.com/apache/tomee.git", "tomee-7.0.x", "tomee-7.0", false).javadoc("^org.apache.(openejb|tomee).*"),
                 // JAKARTA EE
-                new Source("https://github.com/eclipse-ee4j/jakartaee-platform.git", "v10.0", "jakartaee-10.0").related(jakartaEE10).javadoc("^jakarta.*"),
+                new Source("https://github.com/eclipse-ee4j/jakartaee-platform.git", "v10", "jakartaee-10.0").related(jakartaEE10).javadoc("^jakarta.*"),
             //    new Source("https://github.com/eclipse-ee4j/jakartaee-platform.git", "v9.1", "jakartaee-9.1").related(jakartaEE9).javadoc("^jakarta.*"),
            //     new Source("https://github.com/eclipse-ee4j/jakartaee-platform.git", "v8", "jakartaee-8.0").related(jakartaEE8).javadoc("^javax.*"),
                 // JAVA EE : no git source for v7 or less, javadoc is at https://docs.oracle.com/javaee/7/api/
